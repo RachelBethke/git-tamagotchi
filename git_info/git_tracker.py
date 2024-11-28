0999dcf851998bc5a53b521ec4f1962f8a72d344
RachelBethke <rkb76@cornell.edu>
2024-11-27 20:12:39 -0500

    Primary git_tracker.py implementation
    - Implemented a basic git_tracker file base
    - Untested until I implement other files that allow me to test
    - Still a few stubbed functions until I decide

--- git_info/git_tracker.py
+++ git_info/git_tracker.py
@@ -5,31 +5,52 @@ import time
 import json
 import os
 
-GIT_TOKENS = { 
-    # gives access to private and public commit data 
-    'rachelbethke' : 'GITHUB_TOKEN_USERNAME' 
-    #add rkb76 later
-}
+#abstracted for simpler recreation
+#TODO: Simplfiy the process of setting the git user name and token?
+GIT_USERNAME = 'RachelBethke'
+GIT_TOKEN = os.environ.get('GITHUB_TOKEN')
+INTERVAL = 60  # sec between polling (?)
 
-INTERVAL = 60 #seconds between polling (?)
-PUSH_LOG= 'push_events.log'
-LAST_PUSH= "last_event_ids.json"
+# GIT_TOKENS = {  for when I use multiple tokens for school and personal git
+#     # gives access to private and public commit data 
+#     'rachelbethke' : 'GITHUB_TAMA_TOKEN' 
+#     #add rkb76 later
+# }
 
 # push data getter
 def get_last_push():
     """
     Getter to load the last push info from a file
     """
-    if os.path.exists(LAST_PUSH):
-        with open(LAST_PUSH, 'r') as f:
-            return json.load(f)
-    else: return {}
-        
+    headers = { 'authorization' : f'token {GIT_TOKEN}',
+        'Accept': 'application/vnd.github.v3+json',
+    }
+    url = f'https://api.github.com/users/{GIT_USERNAME}/events'
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        print("error fetching")
+        return None
+    events = response.json()
+
+    for i in events:
+        if i['type'] == 'PushEvent':
+            return events
+        return None
+    
 # def save_last_push():
 #     """
 #     Save the last processed push info to a file
 #     """
 #     pass
+def watch_pushes(callback):
+    """ Waching for push events to trigger events when needed"""
+    last_event = None
+    while True:
+        event = get_last_push()
+        if event and event['id'] != last_event:
+            last_event = event['id']
+            callback()
+        time.sleep(INTERVAL)
 
 def process_push(push, un):
     """
@@ -41,3 +62,7 @@ def update_log():
     """ 
     Update git log on the INTERVAL (minute) to reflect state
     """
+    pass
+
+if __name__ == '__main__':
+    watch_pushes(lambda: print("callback"))
