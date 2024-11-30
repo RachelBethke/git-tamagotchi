4490f457fa06a299e118946d03897bcd19127e39
RachelBethke <rkb76@cornell.edu>
2024-11-30 15:51:55 -0500

    Add basic display.py to show a sprite on the desktop and attach to git_tracker.py
    - Created SpriteWidget class in display.py
    - Calling display.py will put the placeholder egg sprite on the screen
    - Sprite stays in front of all other tabs without taking away attention from the tab your working in
    - Added MOCK_PUSH functionality to test sprite reaction to pushes
    - Updated watch_pushes to take a sprite variable to update based on pushes

--- git_info/git_tracker.py
+++ git_info/git_tracker.py
@@ -4,52 +4,91 @@ import requests
 import time
 import json
 import os
+import sys
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+root_dir = os.path.dirname(parent_dir)  # Go up one more level to the project root
+sys.path.insert(0, root_dir)
+from sprites.display import show_sprite, SpriteWidget, QApplication
 
-#abstracted for simpler recreation
-#TODO: Simplfiy the process of setting the git user name and token?
-GIT_USERNAME = 'RachelBethke'
+#TODO: make this a terminal process?
+GIT_USERNAME = 'RachelBethke' #add rkb76 later
 GIT_TOKEN = os.environ.get('GITHUB_TOKEN')
-INTERVAL = 60  # sec between polling (?)
+INTERVAL = 1 # sec between polling (?)
+#GIT_TIKEN = {'rachelbethke' : 'GITHUB_TOKEN'}
+MOCK_PUSH = False
 
-# GIT_TOKENS = {  for when I use multiple tokens for school and personal git
-#     # gives access to private and public commit data 
-#     'rachelbethke' : 'GITHUB_TAMA_TOKEN' 
-#     #add rkb76 later
-# }
+if not GIT_TOKEN:
+    print("Error: GIT_TOKEN is not properly connected.")
+    print("Please set up your token using 'export GITHUB_TOKEN=your_personal_access_token' in the terminal.")
+    sys.exit(1)
 
-# push data getter
-def get_last_push():
+def get_last_push_real():
     """
-    Getter to load the last push info from a file
+    Getter to load the last push info from github
     """
     headers = { 'authorization' : f'token {GIT_TOKEN}',
         'Accept': 'application/vnd.github.v3+json',
     }
     url = f'https://api.github.com/users/{GIT_USERNAME}/events'
+    #print(f"Requesting URL: {url}")
+    #print(f"Using headers: {headers}")
     response = requests.get(url, headers=headers)
+    #print(f"Response status code: {response.status_code}") 
     if response.status_code != 200:
-        print("error fetching")
+        print(f"Error fetching events: {response.status_code}")
+        print(f"Response content: {response.content}")
         return None
     events = response.json()
 
+    # Find the first PushEvent
     for i in events:
         if i['type'] == 'PushEvent':
-            return events
-        return None
-    
+            return i #events (?)
+    print("No PushEvent found")
+    return None
+
+def get_last_push_mock():
+    """
+    Mock push that return a simulated push for testing.
+    """
+    return {
+        'id': str(int(time.time())),  # Unique ID based on current time
+        'type': 'PushEvent',
+        'repo': {'name': 'mock-repo'},
+        'created_at': datetime.datetime.utcnow().isoformat() + 'Z',
+    }
+
+def get_last_push():
+    """
+    Choose between real and mock push implementation.
+    """
+    if MOCK_PUSH:
+        print("Using mock push")
+        return get_last_push_mock()
+    else:
+        print("Getting real push event from GitHub.")
+        return get_last_push_real()
+
 # def save_last_push():
 #     """
 #     Save the last processed push info to a file
 #     """
 #     pass
-def watch_pushes(callback):
-    """ Waching for push events to trigger events when needed"""
+def watch_pushes(sprite_call):
+    app = QApplication.instance()
+    if app is None:
+        app = QApplication(sys.argv)
+
+    sprite = SpriteWidget()
+    sprite.hide()  # Start hidden
     last_event = None
     while True:
         event = get_last_push()
         if event and event['id'] != last_event:
             last_event = event['id']
-            callback()
+            print(f"New push detected: {event}")
+            sprite_call() 
         time.sleep(INTERVAL)
 
 def process_push(push, un):
@@ -65,4 +104,4 @@ def update_log():
     pass
 
 if __name__ == '__main__':
-    watch_pushes(lambda: print("callback"))
+    watch_pushes(show_sprite)
--- sprites/display.py
+++ sprites/display.py
@@ -1,1 +1,60 @@
-# GUI
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt, QTimer
+import sys
+import random
+
+class SpriteWidget(QWidget):
+    def __init__(self, parent=None):
+        super(SpriteWidget, self).__init__(parent)
+
+        # window flags and attributes
+        self.setWindowFlags(
+            Qt.WindowStaysOnTopHint |
+            Qt.FramelessWindowHint |
+            Qt.WindowDoesNotAcceptFocus |
+            Qt.Window
+        )
+        self.setAttribute(Qt.WA_TranslucentBackground)
+        self.setFocusPolicy(Qt.NoFocus)
+
+        # pixmap label creation
+        self.label = QLabel(self)
+        pixmap = QPixmap('sprites/sprite_images/eggSprite.png')
+        if pixmap.isNull():
+            print("sprite didn't load")
+            return
+        else:
+            print("sprite loaded")
+
+        self.label.setPixmap(pixmap)
+        self.label.adjustSize()
+        self.setFixedSize(pixmap.size()) #size of the widget to the size of the pixmap
+        self.move(100, 100) #window pos 
+        self.timer = QTimer()
+        #self.timer.timeout.connect(self.move_sprite)
+        self.timer.start(1000) #moves every second
+
+    # def move_sprite(self):
+    #     screen_geometry = QApplication.primaryScreen().geometry()
+    #     x = random.randint(0, screen_geometry.width() - self.width())
+    #     y = random.randint(0, screen_geometry.height() - self.height())
+    #     self.move(x, y)
+
+    def focusOutEvent(self, event):
+        event.ignore()
+
+def show_sprite():
+    """Show the sprite on home screen window"""
+    # init app
+    app = QApplication.instance()
+    if app is None:
+        app = QApplication(sys.argv)
+
+    sprite = SpriteWidget()
+    sprite.show()
+    app.sprite = sprite #ref so there isn't garbage collection
+    sys.exit(app.exec_()) # event loop
+
+if __name__ == '__main__':
+    show_sprite()
